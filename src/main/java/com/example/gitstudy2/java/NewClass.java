471ca752b2cf9f4157b93586e587703b87d89821
ohsang14 <ast6108@naver.com>
2024-05-05 16:54:42 +0900

    java

---
+++ src/main/java/com/example/gitstudy2/java/NewClass.java
@@ -0,0 +1,8 @@
+package com.example.gitstudy2.java;
+
+public class NewClass {
+    public void test3(){
+        FastCampusClass fastCampusClass  = new FastCampusClass();
+        fastCampusClass.test2();
+    }
+}
