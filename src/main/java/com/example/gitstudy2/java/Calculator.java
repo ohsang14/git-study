25ad1d139b9f3ece0190f9981467e070f3d7f6bd
ohsang14 <ast6108@naver.com>
2024-06-06 17:16:27 +0900

    단위 테스트 프레임워크 알아보기- JUnit5

---
+++ src/main/java/com/example/gitstudy2/java/Calculator.java
@@ -0,0 +1,10 @@
+package com.example.gitstudy2.java;
+
+public class Calculator {
+    public int sum(int a, int b) {
+        if ( a < 0 || b < 0){
+            throw new RuntimeException();
+        }
+        return a + b ;
+    }
+}
